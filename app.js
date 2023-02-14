d9b1516e0d0b3ec256673f7ee74aa10b92ca2315
Ritika <rithu.08.12.1999@gmail.com>
2023-02-14 12:53:41 -0600

    Assignment 1 Solution

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 (function () {
   'use strict';
+  
   angular.module('LunchChecker', [])
   .controller('LunchCheckController', LunchCheckController);
+
   LunchCheckController.$inject = ['$scope'];
   function LunchCheckController($scope){
     $scope.list = "";
     $scope.messageCheck = "Please enter data first";
+
     function message(listSize) {
       if (listSize == 0) {
         return "Please enter data first"
@@ -17,6 +20,7 @@
         return "Too much!"
       }
     };
+
     $scope.checkList = function () {
       var arrayOfList = $scope.list.split(",");
       var listSize = arrayOfList.length
@@ -25,5 +29,6 @@
       }
       $scope.messageCheck=message(listSize);
     };
+
   }
 })();
